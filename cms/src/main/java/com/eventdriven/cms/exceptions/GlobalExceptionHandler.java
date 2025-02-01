3d61cd82f10c54526edbb831e359485dee9c801e
Anwar3006 <anwarsadat.d2@gmail.com>
2025-02-01 19:21:28 +0000

    another change

--- cms/src/main/java/com/eventdriven/cms/exceptions/GlobalExceptionHandler.java
+++ cms/src/main/java/com/eventdriven/cms/exceptions/GlobalExceptionHandler.java
@@ -2,5 +2,4 @@ package com.eventdriven.cms.exceptions;
 
 public class GlobalExceptionHandler {
     
-    
 }
--- react-ui/src/App.tsx
+++ react-ui/src/App.tsx
@@ -3,7 +3,6 @@ import { Button } from "./components/ui/button";
 function App() {
   return (
     <p className="text-3xl">
-      Pop
       <div>
         <Button>Click me now!</Button>
       </div>
